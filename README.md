a5598cf9128926d2db2557aa135a8425d6509cf6
niudt <niudantong.88@gmail.com>
2024-10-20 08:23:26 +0000

    add README.md

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # Gripper_detector
+
+## installation
+
+1. create an environment
+```
+conda create --name gripper_det python=3.8.1
+conda activate gripper_det
+git clone this repo
+```
+2. install torch
+```
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu124
+```
+3. you maybe need to degrade gcc bulabula:
+
+```
+sudo apt install gcc-9 g++-9 gcc-10 g++-10 gcc-11 g++-11 g++-12 gcc-12 g++-13 gcc-13 g++-14 gcc-14 -y
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 70 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
+```
+
+4. install off-the-shelf detectron2
+```
+python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
+```
+
+
+## inference
+just run:
+``` python ./batch_inference.py```
+
+see parser in ```batch_inference.py``` to control if visualize and text_promt and image folder
+
+## supported objects 
+We now only support:
+```
+self.supported_objs = {0: {'id': 0, 'name': 'blue cube', 'color': [220, 20, 60], 'isthing': 1},
+                               1: {'id': 1, 'name': 'green cube', 'color': [220, 20, 60], 'isthing': 1},
+                               2: {'id': 2, 'name': 'magenta cube', 'color': [220, 20, 60], 'isthing': 1},
+                               3: {'id': 3, 'name': 'orange cube', 'color': [220, 20, 60], 'isthing': 1},
+                               4: {'id': 4, 'name': 'purple cube', 'color': [220, 20, 60], 'isthing': 1},
+                               5: {'id': 5, 'name': 'red cube', 'color': [220, 20, 60], 'isthing': 1},
+                               6: {'id': 6, 'name': 'yellow cube', 'color': [220, 20, 60], 'isthing': 1}}
+```
+which means your ```text_prompt``` must be one of them, we can further train a more diverse selector, but the current one should be enough to experiment on "pick cube".
+
+## some demos
+I have run some demos, the results are in ```/scratch/partial_datasets/rlbench/bimanual_data/det_att_pred```
---
+++ inference.py
@@ -0,0 +1,108 @@
+import argparse
+import argparse
+import logging
+import os
+from collections import OrderedDict
+import torch
+import torch.nn as nn
+import random
+import pdb
+import cv2
+import detectron2.utils.comm as comm
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.config import get_cfg
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.engine import DefaultPredictor
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.utils.visualizer import Visualizer
+from detectron2.modeling import build_model
+from detectron2.engine import DefaultTrainer, default_argument_parser, default_setup, hooks, launch
+from detectron2.evaluation import (
+    DatasetEvaluators,
+    PascalVOCDetectionEvaluator,
+    verify_results,
+)
+from torchvision import transforms
+from detectron2.modeling import GeneralizedRCNNWithTTA
+from detectron2.data.datasets import register_coco_instances
+import detectron2.data.transforms as T
+from PIL import Image
+from torchvision.transforms import Compose, Resize, CenterCrop
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+
+class BatchPredictor(nn.Module):
+    """
+    The batch version of detectron2 DefaultPredictor
+    """
+
+    def __init__(self, cfg):
+        super().__init__()
+
+        self.cfg = cfg.clone()  # cfg can be modified by model
+        self.model = build_model(self.cfg)
+        self.model.eval()
+        if len(cfg.DATASETS.TEST):
+            self.metadata = MetadataCatalog.get(cfg.DATASETS.TEST[0])
+
+        checkpointer = DetectionCheckpointer(self.model)
+        checkpointer.load(cfg.MODEL.WEIGHTS)
+
+        self.aug = T.ResizeShortestEdge(
+            [cfg.INPUT.MIN_SIZE_TEST, cfg.INPUT.MIN_SIZE_TEST], cfg.INPUT.MAX_SIZE_TEST
+        )
+
+        self.input_format = cfg.INPUT.FORMAT
+        assert self.input_format in ["RGB", "BGR"], self.input_format
+
+    def forward(self, imgs):
+        with torch.no_grad():  # https://github.com/sphinx-doc/sphinx/issues/4258
+            # Apply pre-processing to image.
+            inputs = []
+            for original_image in imgs:
+                if self.input_format == "RGB":
+                    # whether the model expects BGR inputs or RGB
+                    original_image = original_image[:, :, ::-1]
+                height, width = original_image.shape[:2]
+                image = self.aug.get_transform(original_image).apply_image(original_image)
+                image = torch.as_tensor(image.astype("float32").transpose(2, 0, 1))
+                entry = {"image": image, "height": height, "width": width}
+                inputs.append(entry)
+
+            # inference
+            predictions = self.model(inputs)
+
+            return predictions
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='inference of end-effector detector')
+    parser.add_argument('--config_file', type=str,
+                        default='config/det_config.yaml')
+    parser.add_argument('--ckpt', type=str,
+                        default='/home/niudt/robotic_project/detectron2/tools/rtx_42subset_gripper_new/model_final.pth')
+    parser.add_argument('--device', type=str,
+                        default='cuda')
+    parser.add_argument('--input_img', type=str,
+                        default='/scratch/partial_datasets/rlbench/release_oxe/images/toto/999/92.jpg')
+
+    args = parser.parse_args()
+
+    cfg = get_cfg()
+    cfg.merge_from_file(args.config_file)
+    cfg.MODEL.WEIGHTS = args.ckpt  # add model weight here
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5  # 0.5 , set the testing threshold for this model
+    predictor = BatchPredictor(cfg).to(args.device)
+    supported_objs = {0: {'id': 1, 'name': 'gripper', 'color': [220, 20, 60], 'isthing': 1}}
+
+    # get detection box
+    image_cv2 = cv2.imread(args.input_img)
+    visualizer = Visualizer(img_rgb=image_cv2[:, :, ::-1])
+    image_cv2 = cv2.imread(args.input_img) # Convert from RGB to BGR to align with the image format in OpenCV
+    det_pred = predictor([image_cv2])
+    visualizer.draw_instance_predictions(det_pred)
+
+    # add save_later
+
